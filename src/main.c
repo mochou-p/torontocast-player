62f3e6e4f6f323dc9d28780b503dad1cfd8c3fdb
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-05-11 00:38:46 +0200

    sixel album cover

--- src/main.c
+++ src/main.c
@@ -12,10 +12,16 @@
     #define WHICH_COMMAND_LENGTH 64
 
 
-    int  check_dependencies(void);
-    void print_cell_size   (void);
+    static int check_dependencies(void);
+    static int get_cell_size     (unsigned short *, unsigned short *);
 
     int main(void) {
+        unsigned short cell_width;
+        unsigned short cell_height;
+        unsigned short img_size;
+        unsigned short x_off;
+        char           command[2048];
+
         /* check for bash shell */
         if (0 != system("which bash &> /dev/null")) {
             printf("\x1b[31merror:\x1b[0m `bash` shell is required\n");
@@ -28,12 +34,20 @@
             return 2;
         }
 
-        printf(
-            "\x1b[?1049h"     /* enter alternative screen    */
-            "\x1b[?25l"      /* hide the cursor             */
-            "\x1b[2J"       /* clear the whole screen      */
-            "\x1b[1;1H"    /* move the cursor to top left */
-        );
+        /* calculate size for img2sixel */
+        if (0 != get_cell_size(&cell_width, &cell_height)) {
+            printf("\x1b[31merror:\x1b[0m failed to get terminal sizes\n");
+            return 3;
+        }
+        /* TODO: this will sometimes make a black strip in the sixel image,
+         *       because unsigned short division (pixels/rows),
+         *       use atleast float up until using it
+         */
+        img_size = cell_height * 3;
+        x_off    = img_size / cell_width + 3;
+
+        /* immediately hide the cursor */
+        printf("\x1b[?25l");
         fflush(stdout);
 
         system(
@@ -45,20 +59,30 @@
             "'"
         );
 
-        system(
+        sprintf(
+            command,
             "bash -c '"
                 /* get the json response about the current song from the API */
                 "response=$(curl --silent \"https://kathy.torontocast.com:3310/api/v2/history/?limit=1&offset=0&server=4\");"
                 /* extract the relevant fields */
+                "image=$(echo -n \"${response}\" | grep -m 1 -oP \"\\\"img_large_url\\\":\\\"\\K[^\\\"]+(?=\\\"\\,\\\")\");"
                 "author=$(echo -n \"${response}\" | grep -m 1 -oP \"\\\"author\\\":\\\"\\K[^\\\"]+(?=\\\"\\,\\\")\");"
                 "title=$(echo -n \"${response}\" | grep -m 1 -oP \"\\\"title\\\":\\\"\\K[^\\\"]+(?=\\\"\\,\\\")\");"
                 "album=$(echo -n \"${response}\" | grep -m 1 -oP \"\\\"album\\\":\\\"\\K[^\\\"]+(?=\\\"\\,\\\")\");"
-                /* print them */
-                "echo -e \"\\x1b[34mauthor:\\x1b[0m ${author}\";"
-                "echo -e \"\\x1b[34mtitle:\\x1b[0m  ${title}\";"
-                "echo -e \"\\x1b[34malbum:\\x1b[0m  ${album}\""
-            "'"
+                /* draw album cover */
+                "img2sixel -w %hu -h %hu -I -S -d a_dither -r bilinear -q low -l disable -b xterm256 -E fast $image;"
+                /* print song info */
+                "echo -en \"\\x1b[2A\";"
+                "echo -en \"\\x1b[%huG\\x1b[34mauthor:\\x1b[0m ${author}\";"
+                "echo -en \"\\x1b[1B\";"
+                "echo -en \"\\x1b[%huG\\x1b[34mtitle:\\x1b[0m  ${title}\";"
+                "echo -en \"\\x1b[1B\";"
+                "echo -e  \"\\x1b[%huG\\x1b[34malbum:\\x1b[0m  ${album}\""
+            "'",
+            img_size, img_size,
+            x_off, x_off, x_off
         );
+        system(command);
 
         /* TEMP */
         system("bash -c 'sleep 5'");
@@ -70,18 +94,13 @@
             "'"
         );
 
-        printf(
-            "\x1b[?1049l"   /* leave anternative screen */
-            "\x1b[?25h"    /* show the cursor          */
-        );
-
-        print_cell_size();
-        
+        /* show the cursor */
+        printf("\x1b[?25h");
 
         return 0;
     }
 
-    int check_dependencies(void) {
+    static int check_dependencies(void) {
         static const char *const dependencies[] = {
             "curl", "ffplay", "grep", "img2sixel", "rm", "sleep", "stty"
         };
@@ -94,7 +113,8 @@
         unsigned char len = sizeof(dependencies) / sizeof(dependencies[0]);
         for (; i < len; ++i) {
             if (WHICH_COMMAND_LENGTH <= (strlen(dependencies[i]) + strlen("bash -c 'which  &> /dev/null'"))) {
-                printf("\x1b[31mfatal:\x1b[0m saved from buffer overflow\n");
+                /* cause no snprintf in c89 */
+                printf("\x1b[31mfatal:\x1b[0m saved from sprintf buffer overflow\n");
                 return -1;
             }
 
@@ -110,11 +130,8 @@
         return ret;
     }
 
-    void print_cell_size(void) {
-        struct   winsize ws;
-        unsigned short   cell_width;
-        unsigned short   cell_height;
-
+    static int get_cell_size(unsigned short *width, unsigned short *height) {
+        struct winsize ws;
         ws.ws_col    = 0;
         ws.ws_row    = 0;
         ws.ws_xpixel = 0;
@@ -122,18 +139,18 @@
 
         if (0 != ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)) {
             printf("\x1b[31merror:\x1b[0m `ioctl` returned \"%d: %s\"\n", errno, strerror(errno));
-            return;
+            return 1;
         }
 
         if (0 == (ws.ws_col * ws.ws_row * ws.ws_xpixel * ws.ws_ypixel)) {
             printf("\x1b[31merror:\x1b[0m one of the terminal sizes was invalid\n");
-            return;
+            return 2;
         }
 
-        cell_width  = ws.ws_xpixel / ws.ws_col;
-        cell_height = ws.ws_ypixel / ws.ws_row;
+        *width  = ws.ws_xpixel / ws.ws_col;
+        *height = ws.ws_ypixel / ws.ws_row;
 
-        printf("cell size = %hux%hu\n", cell_width, cell_height);
+        return 0;
     }
 #else
     int main(void) {
